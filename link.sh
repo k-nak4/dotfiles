e5b4412b2d555ad4ee3be7f48c2453bf0b5c9ed8
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2016-08-08 16:38:03 +0900

    add vim

--- link.sh
+++ link.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-#mkdir -p ~/.config/nvim/rc
+mkdir -p ~/.config/nvim/rc
+mkdir -p ~/.vim/rc
 #echo 'export XDG_CONFIG_HOME=$HOME/.config' >> $HOME/.bashrc
 
+# Vim
+ln -sf ~/setting-files/.vimrc ~/.vimrc
+ln -sf ~/setting-files/dein.toml ~/.vim/rc/dein.toml
+ln -sf ~/setting-files/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+
+# NeoVim
 ln -sf ~/setting-files/init.vim ~/.config/nvim/init.vim
 ln -sf ~/setting-files/dein.toml ~/.config/nvim/rc/dein.toml
 ln -sf ~/setting-files/dein_lazy.toml ~/.config/nvim/rc/dein_lazy.toml
