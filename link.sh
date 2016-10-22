2e0a9dacfede9334399d9b9d49af38593a75b81a
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2016-10-22 20:30:46 +0900

    add .zshrc remove init.vim

--- link.sh
+++ link.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir -p ~/.config/nvim/rc
 mkdir -p ~/.vim/rc
 mkdir -p ~/.vim/undo
 
@@ -8,15 +7,11 @@ ln -sf ~/setting-files/.vimrc ~/.vimrc
 ln -sf ~/setting-files/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/setting-files/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 
-# NeoVim
-ln -sf ~/setting-files/init.vim ~/.config/nvim/init.vim
-ln -sf ~/setting-files/init.vim ~/.nvimrc
-ln -sf ~/setting-files/dein.toml ~/.config/nvim/rc/dein.toml
-ln -sf ~/setting-files/dein_lazy.toml ~/.config/nvim/rc/dein_lazy.toml
-#echo 'export XDG_CONFIG_HOME=$HOME/.config' >> $HOME/.bashrc
-
-# Vimparator
+# Vimparator (firefox's add-on)
 ln -sf ~/setting-files/.vimperatorrc ~/.vimperatorrc
+
+# Zsh (oh-my-zsh)
+ln -sf ~/setting-files/.zshrc ~/.zshrc
 
 # Conky
 ln -sf ~/setting-files/.conkyrc ~/.conkyrc
