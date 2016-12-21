2d3187afcb4601adc9fd53f88d243b3bc86427fa
Tamami Wakiyama <Tamami@Coffee.local>
2016-12-21 17:03:41 +0900

    update

--- install.sh
+++ install.sh
@@ -7,8 +7,13 @@ mkdir -p ~/.vim/colors
 ln -sf ~/setting-files/.vimrc ~/.vimrc
 ln -sf ~/setting-files/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/setting-files/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+if [ -e ~/.vim/colors/lucius.vim ]; then
+    echo "lucius.vim is already instaled"
+else
+    wget https://github.com/jonathanfilip/vim-lucius/raw/master/colors/lucius.vim -P ~/.vim/colors
+fi
 
-# Zsh (oh-my-zsh)
+# Zsh
 ln -sf ~/setting-files/.zshrc ~/.zshrc
 
 # Vimparator (firefox's add-on)
